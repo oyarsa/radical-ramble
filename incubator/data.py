03b1174457f98c557b0d5a8b5bb24772e9561f2b
Italo Silva <italo@maleldil.com>
2020-06-25 08:45:57 -0300

    Starts implementing the Dataset. Doing everything manually for now.

--- incubator/data.py
+++ incubator/data.py
@@ -1,8 +1,114 @@
-import pandas as pd
 from pathlib import Path
+from typing import Tuple, NamedTuple, List, TypeVar, Callable, Union, Dict, Set
 
-def load_data(path: Path) -> pd.DataFrame:
-    return pd.read_csv(path)
+import pandas as pd
+import spacy
+import torch
+from torch.utils.data import Dataset
+from spacy.lang.en import English
+
+sentiments = [
+    'positive',
+    'neutral',
+    'negative'
+]
+sentiment2index = {
+    sentiment: index for index, sentiment in enumerate(sentiments)
+}
+
+emotions = [
+    'anger',
+    'disgust',
+    'fear',
+    'joy',
+    'neutral',
+    'surprise',
+    'sadness'
+]
+emotion2index = {
+    emotion: index for index, emotion in enumerate(emotions)
+}
+
+def build_indexes(
+    word_types: Set[str]
+) -> Tuple[Dict[str, int], Dict[int, str]]:
+    word2idx = {'UNK': 0}
+    idx2word = {0: 'UNK'}
+
+    for i, word in enumerate(word_types, 1):
+        word2idx[word] = i
+        idx2word[i] = word
+
+    return word2idx, idx2word
+
+
+def get_word_types(texts: List[List[str]]) -> Set[str]:
+    return set(word for text in texts for word in text)
+
+
+class Vocabulary:
+    def __init__(self, texts: List[List[str]]):
+        self._word_types = get_word_types(texts)
+        self._word2index, self._index2word = build_indexes(self._word_types)
+
+    def word2index(self, word: str) -> int:
+        if word in self._word2index:
+            return self._word2index[word]
+        else:
+            return self._word2index['UNK']
+
+    def index2word(self, index: int) -> str:
+        if index in self._index2word:
+            return self._index2word[index]
+        else:
+            return self._index2word[0]
+
+    def map_tokens_to_ids(self, tokens: List[str]) -> List[int]:
+        return [self.word2index(token) for token in tokens]
+
+    def map_ids_to_tokens(self, ids: List[int]) -> List[str]:
+        return [self.index2word(id) for id in ids]
+
+    def vocab_size(self) -> int:
+        return len(self._word_types)
+
+
+class DatasetRow(NamedTuple):
+    dialogueId: int
+    utteranceId: int
+    tokenIds: torch.Tensor
+    sentimentId: int
+    emotionId: int
+
+class MeldTextDataset(Dataset): # type: ignore
+    def __init__(self, raw_data: Union[pd.DataFrame, Path]):
+        if isinstance(raw_data, Path):
+            raw_data = pd.read_csv(raw_data)
+        self.data = preprocess_data(raw_data)
+        self.token_vocab = Vocabulary(list(self.data.Tokens))
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, index: int) -> DatasetRow:
+        row = self.data.iloc[index]
+        tokenIds = self.token_vocab.map_tokens_to_ids(row.Tokens)
+        return DatasetRow(
+            dialogueId=row.Dialogue_ID,
+            utteranceId=row.Utterance_ID,
+            tokenIds=torch.tensor(tokenIds),
+            sentimentId=sentiment2index[row.Sentiment],
+            emotionId=emotion2index[row.Emotion],
+        )
+
+
+def preprocess_data(data: pd.DataFrame) -> pd.DataFrame:
+    return chain_func(data,
+        clean_unicode,
+        tokenise,
+        remove_punctuation,
+    )
+
 
 def clean_unicode(data: pd.DataFrame) -> pd.DataFrame:
     "Replace the Unicode characters with their appropriate replacements."
@@ -16,3 +122,34 @@ def clean_unicode(data: pd.DataFrame) -> pd.DataFrame:
             .apply(lambda s: s.replace('\x94', ""))
     )
     return data
+
+
+def get_tokeniser() -> spacy.tokenizer.Tokenizer:
+    """
+    Create a Tokenizer with the default settings for English
+    including punctuation rules and exceptions.
+    """
+    nlp = English()
+    tokeniser = nlp.Defaults.create_tokenizer(nlp)
+    return tokeniser
+
+
+def tokenise(data: pd.DataFrame) -> pd.DataFrame:
+    tokeniser = get_tokeniser()
+    data['Tokens'] = data.Utterance.apply(tokeniser)
+    return data
+
+
+def remove_punctuation(data: pd.DataFrame) -> pd.DataFrame:
+    def filter_punct(tokens: spacy.tokens.Doc) -> List[str]:
+        return [token.text for token in tokens if not token.is_punct]
+    data.Tokens = data.Tokens.apply(filter_punct)
+    return data
+
+
+T = TypeVar('T')
+def chain_func(initialArg: T, *functions: Callable[[T], T]) -> T:
+    result = initialArg
+    for function in functions:
+        result = function(result)
+    return result
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,11 +1,91 @@
+import os
+from io import StringIO
 from pathlib import Path
+
+import pandas as pd
+import torch
 
 from incubator import data
 
+test_data_str = """
+Sr No.,Utterance,Speaker,Emotion,Sentiment,Dialogue_ID,Utterance_ID,Season,Episode,StartTime,EndTime
+1,"Oh my God, hes lost it. Hes totally lost it.",Phoebe,sadness,negative,0,0,4,7,"00:20:57,256","00:21:00,049"
+2,What?,Monica,surprise,negative,0,1,4,7,"00:21:01,927","00:21:03,261"
+"""
+test_tokens = [
+    ['Oh', 'my', 'God', 'he', "'s", 'lost', 'it', 'He', "'s",
+     'totally', 'lost', 'it'],
+    ['What'],
+]
+
 def test_clean_unicode() -> None:
     data_path = Path('./data/dev/metadata.csv')
-    dev = data.load_data(data_path)
+    dev = pd.read_csv(data_path)
     dev = data.clean_unicode(dev)
 
     nonascii = dev[dev.Utterance.apply(lambda s: not s.isascii())]
     assert len(nonascii) == 0
+
+def test_preprocessing() -> None:
+    raw_data = StringIO(test_data_str)
+    df = pd.read_csv(raw_data)
+    processed = data.preprocess_data(df)
+
+    assert list(processed.iloc[0].Tokens) == test_tokens[0]
+    assert list(processed.iloc[1].Tokens) == test_tokens[1]
+
+def test_word_types() -> None:
+    raw_data = StringIO(test_data_str)
+    df = pd.read_csv(raw_data)
+    processed = data.preprocess_data(df)
+
+    word_types = data.get_word_types(list(df.Tokens))
+
+    assert all(token in word_types for token in test_tokens[0])
+
+def test_build_indexes() -> None:
+    "Test if every token has an index and the two-way mapping is right"
+    raw_data = StringIO(test_data_str)
+    df = pd.read_csv(raw_data)
+    processed = data.preprocess_data(df)
+    word_types = data.get_word_types(list(df.Tokens))
+
+    word2idx, idx2word = data.build_indexes(word_types)
+
+    for sentence in test_tokens:
+        for token in sentence:
+            assert token in word2idx
+            index = word2idx[token]
+            assert idx2word[index] == token
+
+
+def test_index_uniqueness() -> None:
+    "Test if every token has an unique index"
+    raw_data = StringIO(test_data_str)
+    df = pd.read_csv(raw_data)
+    processed = data.preprocess_data(df)
+    word_types = data.get_word_types(list(df.Tokens))
+
+    word2idx, idx2word = data.build_indexes(word_types)
+
+    for sentence in test_tokens:
+        indexes = [word2idx[token] for token in set(sentence)]
+        assert len(indexes) == len(set(indexes))
+
+
+def test_dataset() -> None:
+    raw_data = StringIO(test_data_str)
+    df = pd.read_csv(raw_data)
+    dataset = data.MeldTextDataset(df)
+
+    assert dataset[0].dialogueId == 0
+    assert dataset[0].utteranceId == 0
+    assert dataset[0].emotionId == data.emotion2index['sadness']
+    assert dataset[0].sentimentId == data.sentiment2index['negative']
+    assert len(dataset[0].tokenIds) == len(test_tokens[0])
+
+    assert dataset[1].dialogueId == 0
+    assert dataset[1].utteranceId == 1
+    assert dataset[1].emotionId == data.emotion2index['surprise']
+    assert dataset[1].sentimentId == data.sentiment2index['negative']
+    assert len(dataset[1].tokenIds) == len(test_tokens[1])
