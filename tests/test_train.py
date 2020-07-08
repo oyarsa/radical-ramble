c49fc8cefcb7e94121f137ac35a1f31b29056111
Italo Silva <italo@maleldil.com>
2020-07-08 17:14:17 -0300

    Adds test for accuracy calculation.

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,13 +1,15 @@
+"""Tests for incubator.train module."""
 from typing import Any
 from io import StringIO
 import wandb
+import torch
 
 import incubator.datasets.meld_linear_text_dataset as mltd
 import incubator.datasets.meld_contextual_text_dataset as mctd
 from incubator.models.simple import glove_simple
 from incubator.models.contextual_simple import glove_contextual_simple
 from incubator.util import set_seed
-from incubator.train import train
+from incubator.train import train, calc_accuracy
 
 import tests.test_models as tm
 from tests.helpers import read_test_data
@@ -18,6 +20,7 @@ def _noop(arg: Any) -> None:
 
 
 def test_linear_train(monkeypatch: Any) -> None:
+    """Test training loop for a linear module."""
     set_seed(1000)
 
     monkeypatch.setattr(wandb, 'log', _noop)
@@ -48,6 +51,7 @@ def test_linear_train(monkeypatch: Any) -> None:
 
 
 def test_contextual_train(monkeypatch: Any) -> None:
+    """Test training loop for a contextual module."""
     set_seed(1000)
 
     monkeypatch.setattr(wandb, 'log', _noop)
@@ -75,3 +79,36 @@ def test_contextual_train(monkeypatch: Any) -> None:
     )
 
     train(model=classifier, trainloader=train_loader, devloader=dev_loader)
+
+
+def test_linear_accuracy() -> None:
+    """Test accuracy calculation for linear model."""
+    # Predictions will be [2, 0, 1]
+    outputs = torch.tensor([
+        [1, 3, 4],
+        [3, 2, 2],
+        [1, 3, 1],
+    ])
+    labels = torch.tensor([2, 1, 1])
+    mask = torch.tensor([1, 1, 1])
+
+    assert calc_accuracy(outputs, labels, mask) == 2
+
+
+def test_contextual_accuracy() -> None:
+    """Test accuracy calculation for contextual model."""
+    # Predictions will be [[2, 0], [1, PAD]]
+    outputs = torch.tensor([
+        [
+            [1, 3, 4],
+            [3, 2, 2],
+        ],
+        [
+            [1, 3, 1],
+            [0, 0, 0],
+        ],
+    ])
+    labels = torch.tensor([[2, 1], [1, 0]])  # last one is padded
+    mask = torch.tensor([[1, 1], [1, 0]])
+
+    assert calc_accuracy(outputs, labels, mask) == 2
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,9 @@
+"""Tests for utility functions."""
 from incubator.util import chain_func, flatten2list
 
+
 def test_flatten_2d() -> None:
+    """Tests flatten2list for a 2D list."""
     lst = [
         [1, 2, 3],
         [4, 5, 6],
@@ -8,9 +11,11 @@ def test_flatten_2d() -> None:
     ]
     flat = flatten2list(lst)
 
-    assert flat == [1,2,3,4,5,6,7,8,9]
+    assert flat == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 
 def test_flatten_3d() -> None:
+    """Tests flatten2list for a 3D list."""
     lst = [
         [
             [1, 2],
@@ -25,7 +30,9 @@ def test_flatten_3d() -> None:
 
     assert flat == [1, 2, 3, 4, 5, 6, 7, 8]
 
+
 def test_flatten_4d() -> None:
+    """Tests flatten2list for a 4D list."""
     lst = [
         [
             [
@@ -51,3 +58,18 @@ def test_flatten_4d() -> None:
     flat = flatten2list(lst)
 
     assert flat == [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def test_chain_func() -> None:
+    """Test chain_func obtains appropriate result."""
+    def multiply_2(x: int) -> int:
+        return x * 2
+
+    result = chain_func(
+        1,
+        multiply_2,
+        multiply_2,
+        multiply_2,
+    )
+
+    assert result == 8
